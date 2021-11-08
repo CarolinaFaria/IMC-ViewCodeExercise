a7a66b51e3bee0e1ed6ab15286f58b372541ca9b
CarolinaFaria <carolina.faria@zup.com.br>
2021-11-08 16:04:46 -0300

    Initial Commit

---
+++ IMC-ViewCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMC-ViewCode
+//
+//  Created by Carolina de Faria Paulo on 08/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
